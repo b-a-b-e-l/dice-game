84485b8fd2eb2fc93fd6f7c543f445a2ed8730e8
b-a-b-e-l <belenbednarski@gmail.com>
2021-02-10 15:43:56 +0100

    personalize images for dice, reworked visuals, added dice function, animations for dice and buttons

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ let player1Turn = true;
 
 const scoreBoard1 = document.getElementById("player1Scoreboard");
 const scoreBoard2 = document.getElementById("player2Scoreboard");
-const player1Dice = document.getElementById("player1Dice");
-const player2Dice = document.getElementById("player2Dice");
+const dices = document.getElementById("dice-box");
 const gameMessage = document.getElementById("message");
 const rollButton = document.getElementById("rollBtn");
 const resetButton = document.getElementById("resetBtn");
+const container = document.getElementById("container");
 
 function endGame() {
   rollButton.style.display = "none";
   resetButton.style.display = "block";
+  container.classList.add("glowreset");
+  resetButton.classList.add("glowreset");
+}
+
+function displayDice(diceValue) {
+  let diceImage = "images/" + diceValue + ".png";
+  dices.innerHTML = `<img src=${diceImage} class="dice" alt="dice number ${diceValue}" />`;
 }
 
 function rollDice() {
@@ -20,24 +27,28 @@ function rollDice() {
   if (player1Turn) {
     player1Score += diceValue;
     scoreBoard1.textContent = player1Score;
-    player1Dice.textContent = diceValue;
-    player1Dice.classList.remove("active");
-    player2Dice.classList.add("active");
+    displayDice(diceValue);
     gameMessage.textContent = "TURN: Player 2";
+    gameMessage.classList.remove("player1");
+    gameMessage.classList.add("player2");
   } else {
     player2Score += diceValue;
     scoreBoard2.textContent = player2Score;
-    player2Dice.textContent = diceValue;
-    player2Dice.classList.remove("active");
-    player1Dice.classList.add("active");
+    displayDice(diceValue);
     gameMessage.textContent = "TURN: Player 1";
+    gameMessage.classList.remove("player2");
+    gameMessage.classList.add("player1");
   }
 
   if (player1Score >= 20) {
     gameMessage.textContent = "WINNER: Player 1!";
+    gameMessage.classList.remove("player1");
+    gameMessage.classList.add("player2");
     endGame();
   } else if (player2Score >= 20) {
     gameMessage.textContent = "WINNER: Player 2!";
+    gameMessage.classList.remove("player2");
+    gameMessage.classList.add("player1");
     endGame();
   }
   player1Turn = !player1Turn;
@@ -47,15 +58,12 @@ function resetGame() {
   player1Score = 0;
   player2Score = 0;
   player1Turn = true;
-  player2Dice.classList.remove("active");
-  player1Dice.classList.add("active");
-  player1Dice.textContent = "-";
-  player2Dice.textContent = "-";
   player1Scoreboard.textContent = player1Score;
   player2Scoreboard.textContent = player2Score;
   gameMessage.textContent = "START: Player 1";
   rollButton.style.display = "block";
   resetButton.style.display = "none";
+  container.classList.remove("glowreset");
 }
 
 rollButton.addEventListener("click", rollDice);
